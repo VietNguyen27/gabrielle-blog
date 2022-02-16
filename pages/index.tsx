50068e59523d1e01b77fc04387b2936d9508b516
VietNguyen27 <nquocviet2307@gmail.com>
2022-02-16 20:47:49 +0700

    Setup project also adds tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Gabrielle</title>
+        <meta
+          name="description"
+          content="Gabrielle is a website which provides Blogging tips, Technology news and reviews, plus you can create your own blog to share interesting knowledge with everyone."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
